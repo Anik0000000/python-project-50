2dc4263db13604578313963d70414dd5fdb27be4
anik0000000 <idozmor@gmail.com>
2025-08-08 14:27:32 +0500

    Make lint

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,13 +1,17 @@
 import os
+
 from gendiff import generate_diff
+
 
 def get_fixture_path(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'fixtures', filename)
 
+
 def read_file(filepath):
     with open(filepath, 'r') as f:
         return f.read().strip()
+
 
 def test_flat_json_diff():
     file1 = get_fixture_path('file1.json')
