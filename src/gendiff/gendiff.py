cfb2f58f79a235ea66ac318e4f130e9616e6bbbd
anik0000000 <idozmor@gmail.com>
2025-08-14 23:04:27 +0500

    Ruff chech

--- src/gendiff/gendiff.py
+++ src/gendiff/gendiff.py
@@ -1,9 +1,12 @@
 import argparse
+
 from gendiff.generate_diff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
     parser.add_argument("first_file", help="Path to the first file")
     parser.add_argument("second_file", help="Path to the second file")
     parser.add_argument(
