28da4e6131b41483a7997931bafc844c451392ad
anik0000000 <idozmor@gmail.com>
2025-08-14 22:50:47 +0500

    Add more new

---
+++ src/gendiff/gendiff.py
@@ -0,0 +1,21 @@
+import argparse
+from gendiff.generate_diff import generate_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first_file", help="Path to the first file")
+    parser.add_argument("second_file", help="Path to the second file")
+    parser.add_argument(
+        "-f", "--format",
+        help="Output format (default: stylish)",
+        default="stylish",
+        choices=["stylish", "plain", "json"],
+    )
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file, args.format)
+    print(diff)
+
+
+if __name__ == "__main__":
+    main()
